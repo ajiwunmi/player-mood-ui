22996a7bb409590577091ffbcf0fe3b8f9bb1187
Ajiwunmi <ajiwunmi@gmail.com>
2025-06-12 19:49:41 +0100

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 // tailwind.config.js
+// export default {
+// 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {
+// 			colors: {
+// 			  dark: "#0F172A",       // background
+// 			  card: "#1E293B",       // card surfaces
+// 			  accent: "#3B82F6",     // buttons/links
+// 			  muted: "#94A3B8",      // text-muted
+// 			  light: "#F8FAFC",      // light text
+// 			},
+// 			fontFamily: {
+// 			  sans: ['"Poppins"', 'sans-serif'],
+// 			},
+// 		  },
+// 	},
+// 	plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-			  dark: "#0F172A",       // background
-			  card: "#1E293B",       // card surfaces
-			  accent: "#3B82F6",     // buttons/links
-			  muted: "#94A3B8",      // text-muted
-			  light: "#F8FAFC",      // light text
-			},
-			fontFamily: {
-			  sans: ['"Poppins"', 'sans-serif'],
-			},
-		  },
+	theme: {  },
 	},
 	plugins: [],
-};
+  }
+  
